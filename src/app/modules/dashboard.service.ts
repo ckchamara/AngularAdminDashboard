d0bfba0541be4fea50c64ac967ad692898416833
Chamarak <ckchamara22@gmail.com>
2020-03-11 11:10:26 +0530

    pichart service give potons by ervice ok

--- src/app/modules/dashboard.service.ts
+++ src/app/modules/dashboard.service.ts
@@ -29,4 +29,79 @@ export class DashboardService {
   cards(){
     return [502, 635, 809, 947, 1402, 3634, 4300];
   }
+
+  pieChartOptions(){
+        return  {
+          chart: {
+              plotBackgroundColor: null,
+              plotBorderWidth: null,
+              plotShadow: false,
+              type: 'pie'
+          },
+          title: {
+              text: 'Random Data'
+          },
+          exporting: {
+            enabled: true
+          },
+          credits: {
+            enabled: false
+          },
+          tooltip: {
+              pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+          },
+          accessibility: {
+              point: {
+                  valueSuffix: '%'
+              }
+          },
+          plotOptions: {
+              pie: {
+                  allowPointSelect: true,
+                  cursor: 'pointer',
+                  dataLabels: {
+                      enabled: true,
+                      format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                  }
+              }
+          },
+          series: [{
+              name: 'Brands',
+              colorByPoint: true,
+              data: [{
+                  name: 'Chrome',
+                  y: 61.41,
+                  sliced: true,
+                  selected: true
+              }, {
+                  name: 'Internet Explorer',
+                  y: 11.84
+              }, {
+                  name: 'Firefox',
+                  y: 10.85
+              }, {
+                  name: 'Edge',
+                  y: 4.67
+              }, {
+                  name: 'Safari',
+                  y: 4.18
+              }, {
+                  name: 'Sogou Explorer',
+                  y: 1.64
+              }, {
+                  name: 'Opera',
+                  y: 1.6
+              }, {
+                  name: 'QQ',
+                  y: 1.2
+              }, {
+                  name: 'Other',
+                  y: 2.61
+              }]
+          }]
+      };
+   }
+
+
+
 }
--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -10,11 +10,13 @@ export class DashboardComponent implements OnInit {
 
   bigChart = [];
   cards = [];
+  pieChartOptions = {};
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
     this.bigChart = this.dashboardService.bigChart();
     this.cards = this.dashboardService.cards();
+    this.pieChartOptions = this.dashboardService.pieChartOptions();
   }
 
 }
